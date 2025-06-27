b5e20405509a638bf04270fca27222bfd10268fe
William Suriaputra <willisurya2005@gmail.com>
2025-06-26 21:47:04 -0400

    cleanup on the embedder flow

--- pdf2embed.py
+++ pdf2embed.py
@@ -1,33 +1,36 @@
 from agno.knowledge.pdf import PDFKnowledgeBase, PDFReader
 from agno.vectordb.pgvector import PgVector
 from agno.embedder.openai import OpenAIEmbedder
-# from agno.embedder.google import GeminiE
-# mbedder
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-API_KEY = os.getenv("EMBEDDINGS_MODEL_APIKEY")
-pdf_knowledge_base = PDFKnowledgeBase(
-    path="downloaded_pdfs",
-    # Table name: ai.pdf_documents
-    vector_db=PgVector(
-        table_name="pdf_documents",
-        # table_name="gemini_embeddings",
-        db_url=os.getenv("EMBEDDINGS_STORAGE"),
-        # embedder=GeminiEmbedder(
-        #     api_key=API_KEY, 
-        #     task_type="RETRIEVAL_DOCUMENT",
-        #     id="gemini-embedding-exp-03-07"),
-        embedder=OpenAIEmbedder(id="nomic-embed-text",
-                                dimensions=768,
-                                base_url=os.getenv("EMBEDDINGS_MODEL_HOST"),
-                                api_key=API_KEY)
-    ),
-    reader=PDFReader()
-)
+def parse_pdfs(reload=False):
+    load_dotenv()
 
-# pdf_knowledge_base.load(recreate=True)
+    API_KEY = os.getenv("EMBEDDINGS_MODEL_APIKEY")
+    pdf_knowledge_base = PDFKnowledgeBase(
+        path="downloaded_pdfs",
+        # Table name: ai.pdf_documents
+        vector_db=PgVector(
+            table_name="pdf_documents",
+            db_url=os.getenv("EMBEDDINGS_STORAGE"),
+            embedder=OpenAIEmbedder(id="nomic-embed-text",
+                                    dimensions=768,
+                                    base_url=os.getenv("EMBEDDINGS_MODEL_HOST"),
+                                    api_key=API_KEY)
+        ),
+        reader=PDFReader()
+    )
 
-# try gemini embedder
+    if reload:
+        try:
+            while True:
+                # can comment after the first run
+                pdf_knowledge_base.load(recreate=False)
+                break
+        except Exception as e:
+            pass
+
+if __name__ == "__main__":
+    parse_pdfs(reload=True)
